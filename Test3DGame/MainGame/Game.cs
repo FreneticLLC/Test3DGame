55c8244dbf2c91794e333366f7ad4bbc09f12930
mcmonkey4eva <agoodwin@freneticllc.com>
2017-08-16 01:37:38 -0700

    Add program initial setup, game placeholder

---
+++ Test3DGame/MainGame/Game.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3DGame.MainGame
+{
+    /// <summary>
+    /// The game's entry point.
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Starts the game.
+        /// </summary>
+        public void Start()
+        {
+
+        }
+    }
+}
---
+++ Test3DGame/T3DProgram.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using FreneticGameCore;
+using Test3DGame.MainGame;
+
+namespace Test3DGame
+{
+    /// <summary>
+    /// Central entry class for the program.
+    /// </summary>
+    public class T3DProgram : Program
+    {
+        /// <summary>
+        /// The name of the game.
+        /// </summary>
+        public const string T3DGameName = "Test3DGame";
+
+        /// <summary>
+        /// The version of the game. Automatically read from file.
+        /// </summary>
+        public static readonly string T3DGameVersion = Assembly.GetCallingAssembly().GetName().Version.ToString();
+
+        /// <summary>
+        /// The description of the game version.
+        /// </summary>
+        public const string T3dVersionDescription = "Pre-Alpha";
+
+        public T3DProgram()
+            : base(T3DGameName, T3DGameVersion, T3dVersionDescription)
+        {
+        }
+
+        /// <summary>
+        /// Main entry method.
+        /// </summary>
+        /// <param name="args">Command line arguments, if any.</param>
+        static void Main(string[] args)
+        {
+            PreInit(new T3DProgram());
+            SysConsole.Init();
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
